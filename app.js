17f1a628019cd9cbefe5e0314fba35efd10b17a5
mikareich <mikareich0@gmail.com>
2020-10-21 20:01:05 +0200

    vn gfjmk,ds

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const song = []
+let record = true
+let startTimestamp = new Date().getTime()
+
 const soundFrequency = {
   c: '261.6',
   cis: '277.2',
@@ -13,7 +17,9 @@ const soundFrequency = {
   h: '493.9'
 }
 
-const oscillators = Object.keys(soundFrequency).map(setOscillator)
+const oscillators = Object.entries(
+  soundFrequency
+).map(([sound, frequency]) => ({ sound, ...setOscillator(frequency) }))
 
 function setOscillator(frequency) {
   const audioContext = new AudioContext()
@@ -26,4 +32,55 @@ function setOscillator(frequency) {
   }
 }
 
-window.addEventListener('keypress', ({ key, shiftedKey }) => {})
+function playSong(song) {
+  song.forEach(({ sound, time, action }) => {
+    setTimeout(() => playSound(sound, action), time)
+  })
+}
+
+function updateSong(sound, action) {
+  const passedTime = new Date().getTime() - startTimestamp
+  song.push({
+    time: passedTime,
+    sound,
+    action
+  })
+  console.clear()
+  console.table(song)
+}
+
+function playSound(sound, action) {
+  const soundHTMLElement = document.getElementById(sound)
+  const oscillator = oscillators.find((o) => o.sound === sound)
+  if (!oscillator) return
+  if (action === 'start') {
+    oscillator.start()
+    soundHTMLElement.classList.add('selected')
+  } else {
+    oscillator.stop()
+    soundHTMLElement.classList.remove('selected')
+  }
+}
+
+oscillators.forEach(({ sound }) => {
+  const soundHTMLElement = document.getElementById(sound)
+  const onTriggered = ({ type }) => {
+    const action = type === 'mousedown' ? 'start' : 'stop'
+    playSound(sound, action)
+    record && updateSong(sound, action)
+  }
+  soundHTMLElement.onmousedown = onTriggered
+  soundHTMLElement.onmouseup = onTriggered
+})
+
+function onKeyboard(keyboard, action) {
+  const { key, shiftKey } = keyboard
+  const sound = shiftKey ? key.toLowerCase() + 'is' : key.toLowerCase()
+
+  playSound(sound, action)
+
+  record && updateSong(sound, action)
+}
+
+window.addEventListener('keypress', (e) => onKeyboard(e, 'start'))
+window.addEventListener('keyup', (e) => onKeyboard(e, 'stop'))
