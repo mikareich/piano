b406f30c8112d10c569ac9cc5ebe336b1ed8b0ab
mikareich <mikareich0@gmail.com>
2020-10-21 15:00:06 +0200

    uff

--- app.js
+++ app.js
@@ -1,74 +1,29 @@
-const notes = [
-  "c",
-  "cis",
-  "d",
-  "dis",
-  "e",
-  "f",
-  "fis",
-  "g",
-  "gis",
-  "a",
-  "ais",
-  "h"
-];
+const soundFrequency = {
+  c: '261.6',
+  cis: '277.2',
+  d: '293.7',
+  dis: '311.1',
+  e: '329.6',
+  f: '349.2',
+  fis: '370.0',
+  g: '392.0',
+  gis: '415.3',
+  a: '440.0',
+  ais: '466.2',
+  h: '493.9'
+}
 
-const keysDIV = document.querySelectorAll(".key");
-const recordTriggerBTN = document.getElementById("recordTrigger");
+const oscillators = Object.keys(soundFrequency).map(setOscillator)
 
-let recordedSong = [];
-let lastTimeStamp = 0;
-let recordStatus = false;
-
-// register event listener for divs
-keysDIV.forEach(el =>
-  el.addEventListener("click", () => linkToAudio(el.dataset.note))
-);
-// register event listener for keyboard
-window.addEventListener("keypress", e => {
-  // get note from key
-  const note =
-    e.key === e.key.toUpperCase() ? e.key.toLowerCase() + "is" : e.key;
-  linkToAudio(note);
-});
-
-recordTriggerBTN.addEventListener("click", function() {
-  recordStatus = !recordStatus;
-  // clear song
-  if(!recordStatus) recordedSong = []
-  this.textContent = recordStatus ? "STOP" : "RECORD";
-  lastTimeStamp = new Date().getTime();
-});
-
-function linkToAudio(note) {
-  if (notes.includes(note)) {
-    // toggle class to html key
-    const key = [...keysDIV].find(key => key.dataset.note === note);
-    key.classList.add("active")
-    // if song is record, add it to song
-    if(recordStatus) record(note)
-    // create audio element and play it
-    const audioElement = document.createElement("audio");
-    audioElement.src = `./notes/${note}.mp3`;
-    // add element to hook
-    document.body.appendChild(audioElement);
-    audioElement.play();
-    // delete element after playing
-    audioElement.addEventListener("ended", function() {
-      key.classList.remove("active")
-      this.remove();
-    });
-  } else {
-    console.error(`[link to audio] selected note does not exist.`);
+function setOscillator(frequency) {
+  const audioContext = new AudioContext()
+  const oscillator = audioContext.createOscillator()
+  oscillator.frequency.value = frequency
+  oscillator.start()
+  return {
+    start: () => oscillator.connect(audioContext.destination),
+    stop: () => oscillator.disconnect()
   }
 }
 
-function record(note) {
-  const newTimestamp = new Date().getTime()
-  const timestamp = newTimestamp - lastTimeStamp
-  lastTimeStamp = newTimestamp
-  recordedSong.push({
-    note,
-    timestamp
-  })
-}
+window.addEventListener('keypress', ({ key, shiftedKey }) => {})
